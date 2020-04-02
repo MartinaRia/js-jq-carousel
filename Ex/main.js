9a23e6ed32cefe2f8b977860fca879d4f442f882
Martina <martina.ria@hotmail.com>
2020-04-02 18:04:46 +0200

    Aggiunta classe per collegare ball to img

--- Ex/main.js
+++ Ex/main.js
@@ -1,66 +1,88 @@
 // creare uno slider
 $(document).ready(function(){
 
+  //EVENTI AL CLICK
 
-// gestione evento sul prev
- $('.prev').click(
-   //funzione di CB
-     prevImg
- );
-
-// gestione evento sul next
- $('.next').click(
-   //funzione di CB
+    // freccia next
+   $('.next').click(
      nextImg
- );
+   );
+
+   // freccia prev
+    $('.prev').click(
+        prevImg
+    );
+
+    //click sul pallino
 
 
- // funzione next
-function nextImg() {
-
-   // salvo ref a img attiva al momento del click
-   var imgActive = $('.images img.active');
-   // salvo il pallino attivo
-   var ballActive = $('.nav i.active');
-
-   // tolgo la classe active all'img selezionata
-   imgActive.removeClass('active');
-   // tolgo la classe active al pallino selezionato
-   ballActive.removeClass('active');
 
 
-   // verifico se questa img era l'ultima
-   if(imgActive.hasClass('last')){
-     $('.images img.first').addClass('active');
-     $('.nav i.first').addClass('active');
-   } else {
-     // applica classe active alla prox img
-     imgActive.next().addClass('active');
-     ballActive.next().addClass('active');
-   }
-}
+  //FUNZIONI
 
-// funzione prevImg
-function prevImg() {
+   // funzione NEXT --------------------------------
+  function nextImg() {
 
-  // salvo ref a img e ball attiva al momento del click
-  var imgActive = $('.images img.active');
-  var ballActive = $('.nav i.active');
-  // tolgo la classe active all'img e ball selezionati
-  imgActive.removeClass('active');
-  ballActive.removeClass('active');
+     // salvo ref a img attiva al momento del click
+     var imgActive = $('.images img.active');
+     // salvo il pallino attivo
+     var ballActive = $('.nav i.active');
 
-  // verifico se questa img era la prima
-  if(imgActive.hasClass('first')){
-    $('.images img.last').addClass('active');
-    $('.nav i.last').addClass('active');
-  } else {
-    // applica classe active alla prox img
-    imgActive.prev().addClass('active');
-    ballActive.prev().addClass('active');
+     // tolgo la classe active all'img selezionata
+     imgActive.removeClass('active');
+     // tolgo la classe active al pallino selezionato
+     ballActive.removeClass('active');
+
+
+     // verifico se questa img era l'ultima
+     if(imgActive.hasClass('last')){
+       $('.images img.first').addClass('active');
+       $('.nav i.first').addClass('active');
+     } else {
+       // applica classe active alla prox img
+       imgActive.next().addClass('active');
+       ballActive.next().addClass('active');
+     }
   }
 
-}
+  // funzione PREV ---------------------------------------
+  function prevImg() {
+
+    // salvo ref a img e ball attiva al momento del click
+    var imgActive = $('.images img.active');
+    var ballActive = $('.nav i.active');
+    // tolgo la classe active all'img e ball selezionati
+    imgActive.removeClass('active');
+    ballActive.removeClass('active');
+
+    // verifico se questa img era la prima
+    if(imgActive.hasClass('first')){
+      $('.images img.last').addClass('active');
+      $('.nav i.last').addClass('active');
+    } else {
+      // applica classe active alla prox img
+      imgActive.prev().addClass('active');
+      ballActive.prev().addClass('active');
+    }
+
+  }
+
+  var imgOfImages = $('.images img');
+
+  for (var i = 0; i <= imgOfImages.length; i++) {
+    //assegnare classe a immagini
+    var selectorImg = '.images img:nth-child'+'('+ i +')';
+    var classToAddImg = 'img'+i
+    $(selectorImg).addClass(classToAddImg);
+
+    //assegnare classe a ball
+    var selectorBall = '.nav i:nth-child'+'('+ i +')';
+    var classToAddBall = 'ball'+i
+    $(selectorBall).addClass(classToAddBall);
+
+  }
+
+
 
 
 
